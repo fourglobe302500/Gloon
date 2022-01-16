8d3d77c1514f58d0d04b32b587680b317b8aabfc
fourglobe302500 <franciscomiguelolima@gmail.com>
2022-01-16 10:47:14 +0000

    Adding char lexing

--- src/Gloon/Lexer.fs
+++ src/Gloon/Lexer.fs
@@ -1,3 +1,50 @@
+[<RequireQualifiedAccess>]
 module Gloon.Lexer
 
-let lex s = s
+open System
+
+let private (|Letter|_|) c =
+  if Char.IsLetter c then
+    Some <| string c
+  else
+    None
+
+let private (|Number|_|) c =
+  if Char.IsNumber c then
+    Some(int c - int '0', string c)
+  else
+    None
+
+let private (|WhiteSpace|_|) c =
+  if Char.IsWhiteSpace c then
+    Some <| string c
+  else
+    None
+
+let private map i =
+  function
+  | Letter l -> Identifier(SyntaxNode.Basic l (i + 1))
+  | Number (n, t) -> Literal(SyntaxNode.Basic t (i + 1), Int n)
+  | WhiteSpace w -> WhiteSpace <| SyntaxNode.Basic w (i + 1)
+  | '\n' -> WhiteSpace <| SyntaxNode.Basic "\n" (i + 1)
+  | '\r' -> WhiteSpace <| SyntaxNode.Basic "\r" (i + 1)
+  | '.' -> Dot <| SyntaxNode.Basic "." (i + 1)
+  | '+' -> Plus <| SyntaxNode.Basic "+" (i + 1)
+  | '-' -> Minus <| SyntaxNode.Basic "-" (i + 1)
+  | '*' -> Star <| SyntaxNode.Basic "*" (i + 1)
+  | '/' -> Slash <| SyntaxNode.Basic "/" (i + 1)
+  | '\\' -> BackSlash <| SyntaxNode.Basic "\\" (i + 1)
+  | '%' -> Percent <| SyntaxNode.Basic "%" (i + 1)
+  | '=' -> Equals <| SyntaxNode.Basic "=" (i + 1)
+  | '<' -> LessThan <| SyntaxNode.Basic "<" (i + 1)
+  | '>' -> GreaterThan <| SyntaxNode.Basic ">" (i + 1)
+  | '!' -> ExclamationMark <| SyntaxNode.Basic "!" (i + 1)
+  | '?' -> QuestionMark <| SyntaxNode.Basic "?" (i + 1)
+  | '|' -> Pipe <| SyntaxNode.Basic "|" (i + 1)
+  | '&' -> Ampersant <| SyntaxNode.Basic "&" (i + 1)
+  | '\'' -> Quote <| SyntaxNode.Basic "'" (i + 1)
+  | '"' -> DoubleQuote <| SyntaxNode.Basic "\"" (i + 1)
+  | c -> Bad <| SyntaxNode.Basic(c.ToString()) (i + 1)
+
+
+let lex (s: string) = List.ofSeq s |> List.mapi map
--- src/Gloon/Types.fs
+++ src/Gloon/Types.fs
@@ -5,8 +5,14 @@ type Span =
   { Start: int
     Length: int }
   member t.End = t.Start + t.Length
+  static member FromBounds s e = { Start = s; Length = e - s }
 
-type SyntaxNode = { Span: Span; Text: string }
+type SyntaxNode =
+  { Span: Span
+    Text: string }
+  static member Basic t i =
+    { Text = t
+      Span = { Start = i; Length = t.Length } }
 
 type LiteralValue =
   | Int of int
@@ -23,13 +29,14 @@ type SyntaxToken =
 
   // Value Tokens
   | Literal of SyntaxNode * LiteralValue
-  | Identifier of SyntaxNode * string
+  | Identifier of SyntaxNode
 
   //Operator Tokens
   | Plus of SyntaxNode
   | Minus of SyntaxNode
   | Star of SyntaxNode
   | Slash of SyntaxNode
+  | BackSlash of SyntaxNode
   | Percent of SyntaxNode
   | Equals of SyntaxNode
   | LessThan of SyntaxNode
@@ -65,3 +72,93 @@ type SyntaxToken =
   | If of SyntaxNode
   | Then of SyntaxNode
   | Else of SyntaxNode
+
+[<RequireQualifiedAccess>]
+module SyntaxToken =
+  let text =
+    function
+    | EOF -> ""
+    | Bad node -> node.Text
+    | WhiteSpace node -> node.Text
+    | Literal (node, _) -> node.Text
+    | Identifier node -> node.Text
+    | Plus node -> node.Text
+    | Minus node -> node.Text
+    | Star node -> node.Text
+    | Slash node -> node.Text
+    | BackSlash node -> node.Text
+    | Percent node -> node.Text
+    | Equals node -> node.Text
+    | LessThan node -> node.Text
+    | GreaterThan node -> node.Text
+    | ExclamationMark node -> node.Text
+    | QuestionMark node -> node.Text
+    | Pipe node -> node.Text
+    | Ampersant node -> node.Text
+    | Dot node -> node.Text
+    | Comma node -> node.Text
+    | Quote node -> node.Text
+    | DoubleQuote node -> node.Text
+    | OpenParen node -> node.Text
+    | CloseParen node -> node.Text
+    | OpenBracket node -> node.Text
+    | CloseBracket node -> node.Text
+    | OpenSquareBracket node -> node.Text
+    | CloseSquareBracket node -> node.Text
+    | Power node -> node.Text
+    | NotEquals node -> node.Text
+    | LessThanOrEquals node -> node.Text
+    | GreaterThanOrEquals node -> node.Text
+    | And node -> node.Text
+    | Or node -> node.Text
+    | For node -> node.Text
+    | While node -> node.Text
+    | Until node -> node.Text
+    | Do node -> node.Text
+    | If node -> node.Text
+    | Then node -> node.Text
+    | Else node -> node.Text
+
+  let span =
+    function
+    | EOF -> Span.FromBounds 0 0
+    | Bad node -> node.Span
+    | WhiteSpace node -> node.Span
+    | Literal (node, _) -> node.Span
+    | Identifier node -> node.Span
+    | Plus node -> node.Span
+    | Minus node -> node.Span
+    | Star node -> node.Span
+    | Slash node -> node.Span
+    | BackSlash node -> node.Span
+    | Percent node -> node.Span
+    | Equals node -> node.Span
+    | LessThan node -> node.Span
+    | GreaterThan node -> node.Span
+    | ExclamationMark node -> node.Span
+    | QuestionMark node -> node.Span
+    | Pipe node -> node.Span
+    | Ampersant node -> node.Span
+    | Dot node -> node.Span
+    | Comma node -> node.Span
+    | Quote node -> node.Span
+    | DoubleQuote node -> node.Span
+    | OpenParen node -> node.Span
+    | CloseParen node -> node.Span
+    | OpenBracket node -> node.Span
+    | CloseBracket node -> node.Span
+    | OpenSquareBracket node -> node.Span
+    | CloseSquareBracket node -> node.Span
+    | Power node -> node.Span
+    | NotEquals node -> node.Span
+    | LessThanOrEquals node -> node.Span
+    | GreaterThanOrEquals node -> node.Span
+    | And node -> node.Span
+    | Or node -> node.Span
+    | For node -> node.Span
+    | While node -> node.Span
+    | Until node -> node.Span
+    | Do node -> node.Span
+    | If node -> node.Span
+    | Then node -> node.Span
+    | Else node -> node.Span
